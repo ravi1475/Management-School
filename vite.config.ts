1e5f3a6f80c247e2f53d146005274680d8745a8a
ravi1475 <rakshitruhil270@gmail.com>
2025-02-27 12:12:18 +0530

    hello

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,40 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes('node_modules')) {
+//             return id
+//               .toString()
+//               .split('node_modules/')[1]
+//               .split('/')[0]
+//               .toString();
+//           }
+//         },
+//       },
+//     },
+//     chunkSizeWarningLimit: 1000, // Adjust this value to set the warning limit
+//   },
+// });
+
+
+
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -15,6 +52,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
+      external: ['recharts'],
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
@@ -30,3 +68,4 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000, // Adjust this value to set the warning limit
   },
 });
+
