09a61d4d2db64f11ff1144cc34fef85edd026f59
ravi1475 <rakshitruhil270@gmail.com>
2025-02-27 12:08:23 +0530

    hello

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,20 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust this value to set the warning limit
+  },
 });
